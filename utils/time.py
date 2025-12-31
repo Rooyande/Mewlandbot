e99f6e4e9f1f317c56a1be6ee3af67a9501e8cf4
moahmmad javad rooyande <mjrooyande@gmail.com>
2025-12-31 19:20:11 +0330

    Update time.py

--- utils/time.py
+++ utils/time.py
@@ -1,1 +1,13 @@
+import time
+
+
+def now_ts() -> int:
+    return int(time.time())
+
+
+def format_mm_ss(seconds: int) -> str:
+    seconds = max(0, int(seconds))
+    m = seconds // 60
+    s = seconds % 60
+    return f"{m} دقیقه و {s} ثانیه"
 
