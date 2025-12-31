caf6ea99a4ab188eb754ced2959efc318630b4d3
moahmmad javad rooyande <mjrooyande@gmail.com>
2025-12-31 19:22:10 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,10 @@
 from aiogram.dispatcher import Dispatcher
 from handlers import start
+from handlers import mew
+from handlers import profile
 
 
 def register_all(dp: Dispatcher):
     start.register(dp)
-
+    mew.register(dp)
+    profile.register(dp)
