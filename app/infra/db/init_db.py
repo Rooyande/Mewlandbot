644eae2003352c835a814af0b3d00a20bd0b1e49
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-06 20:09:57 +0330

    Update init_db.py

--- app/infra/db/init_db.py
+++ app/infra/db/init_db.py
@@ -1,13 +1,19 @@
 import asyncio
 
 from app.infra.db.session import engine
-from app.domain.users.models import Base
+
+# Base مشترک
+from app.domain.users.models import Base  # noqa: F401
+
+# فقط برای اینکه مدل‌ها import شوند و Base.metadata پر شود:
+from app.domain.users.models import User  # noqa: F401
+from app.domain.cats.models import Cat, UserCat  # noqa: F401
 
 
 async def init_db() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    print("Tables created")
+    print("Tables created/updated")
 
 
 if __name__ == "__main__":
