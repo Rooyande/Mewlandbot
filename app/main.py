7ecfe975e468d3419edf8b280be6069383dad9cd
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-06 20:18:58 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 import asyncio
+import logging
 
-from app.bootstrap import run_bot
+from aiogram import Bot, Dispatcher
+
+from app.config.settings import settings
+from app.bot.routers.public import router as public_router
+from app.bot.routers.admin import router as admin_router
+from app.bot.routers.cats import router as cats_router
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main() -> None:
+    bot = Bot(token=settings.bot_token)
+    dp = Dispatcher()
+
+    # routers
+    dp.include_router(admin_router)
+    dp.include_router(cats_router)
+    dp.include_router(public_router)
+
+    # polling
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(run_bot())
-
+    asyncio.run(main())
