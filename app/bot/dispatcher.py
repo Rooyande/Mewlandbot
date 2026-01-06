03f861406d69d89576f646cc893779da17cfebba
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-06 18:22:52 +0330

    Update dispatcher.py

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -1,1 +1,11 @@
+from aiogram import Dispatcher
+
+from app.bot.routers import public, admin
+
+
+def setup_dispatcher() -> Dispatcher:
+    dp = Dispatcher()
+    dp.include_router(admin.router)
+    dp.include_router(public.router)
+    return dp
 
