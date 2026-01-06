edb9421840b5f2fa4092cd8156957cece92cfbb6
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-06 22:17:26 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.bot.routers.public import router as public_router
 from app.bot.routers.admin import router as admin_router
 from app.bot.routers.admin_cats import router as admin_cats_router
 from app.bot.routers.cats import router as cats_router
-
+from app.bot.routers.shop import router as shop_router  # ✅ NEW
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,13 +17,14 @@ logging.basicConfig(level=logging.INFO)
 async def main() -> None:
     bot = Bot(token=settings.bot_token)
 
-    # ✅ FSM Storage (برای setcatpic و حالت‌های آینده)
+    # ✅ FSM Storage
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
 
-    # routers
+    # routers (ترتیب مهم نیست، ولی admin بهتره بالا باشه)
     dp.include_router(admin_router)
     dp.include_router(admin_cats_router)
+    dp.include_router(shop_router)      # ✅ NEW
     dp.include_router(cats_router)
     dp.include_router(public_router)
 
